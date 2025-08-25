2c9aa5e26ac3d477e30e7b9b36ba55f36cc72e36
Kushagra <kushagraa2004@gmail.com>
2025-08-25 13:51:08 +0530

    calculated daily returns and portfolio volatility

--- portfolio_risk.py
+++ portfolio_risk.py
@@ -6,3 +6,15 @@ stocks = ["AAPL", "MSFT", "GOOGL"]
 data = yf.download(stocks, start="2020-01-01", end="2024-12-31")["Close"]
 
 print(data.head())
+
+returns = data.pct_change().dropna()
+
+weights = [1/len(stocks)] * len(stocks)  
+portfolio_returns = (returns @ weights)
+
+
+avg_return = portfolio_returns.mean()
+volatility = portfolio_returns.std()
+
+print("Average Daily Return:", round(avg_return, 5))
+print("Volatility (Std Dev):", round(volatility, 5))
